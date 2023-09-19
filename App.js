5e9c57373019f6847117d042ddc4f26009505949
sssrio <shuhaila@springrain.io>
2023-09-19 17:15:24 +0600

    fix linting

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { multiply } from "react-native-vt"; 
 
 export default function App() {
+  const [result, setResult] = React.useState(0);
+  React.useEffect(() => {
+    multiply(3, 7).then(setResult);
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!{result}</Text>
       <StatusBar style="auto" />
     </View>
   );
